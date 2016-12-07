974edc216fe1b9c59cbeeab1a7cb1f734f9e60ec
jhnmeelr <jhn.meelr@gmail.com>
2016-12-07 23:45:55 +0200

    final

--- server/server.js
+++ server/server.js
@@ -3,9 +3,33 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.status(200).send([
+        {
+            name: 'a',
+            age: '1'
+        },
+        {
+            name: 'b',
+            age: '2'
+        },
+        {
+            name: 'c',
+            age: '3'
+        }
+    ]);
 });
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
+
+module.exports = {
+    app
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,17 +2,34 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
 
-    expect(res).toBe(44).toBeA('number');
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+            expect(res).toBe(44).toBeA('number');
+        });
+    });
+
+    it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    })
+    it('should add square a number', () => {
+        var res = utils.square(4);
+        expect(res).toBe(16).toBeA('number');
+    });
+    it('should async square a number', (done) => {
+        utils.asyncSquare(3, (square) => {
+            expect(square).toBe(9).toBeA('number');
+            done();
+        })
+    });
+});
 });
 
-it('should add square a number', () => {
-    var res = utils.square(4);
 
-    expect(res).toBe(16).toBeA('number');
-});
 
 it('should set firstName and lastName', () => {
     var someUser = { location: 'Philadelphia', age: 25 }
@@ -22,20 +39,6 @@ it('should set firstName and lastName', () => {
         firstName: 'Andrew',
         lastName: 'Mead'
     }).toBeA('object');
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
-    })
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(3, (square) => {
-        expect(square).toBe(9).toBeA('number');
-        done();
-    })
 });
 
 // it('should expect some values', () => {
